f371b3748a31eb9bd3a2e395ccc08634f68f1643
rr222cy <rr222cy@student.lnu.se>
2014-10-02 14:07:08 +0200

    Repository.php added to models

---
+++ src/models/Repository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace models;
+
+require_once('Settings.php');
+
+abstract class Repository {
+    protected $dbConnection;
+    protected $dbTable;
+
+    protected function connection() {
+
+        if($this->dbConnection == NULL)
+        {
+            $this->dbConnection = new \PDO(\Settings::$DB_CONNECTION, \Settings::$DB_USERNAME, \Settings::$DB_PASSWORD);
+        }
+
+        $this->dbConnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $this->dbConnection;
+    }
+}
