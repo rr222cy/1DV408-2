b44575a8d49c56bc22b0c9d638082cc071503a83
rr222cy <rr222cy@student.lnu.se>
2014-10-03 13:06:45 +0200

    Update of database dump

--- mysql/database.sql
+++ mysql/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 30 sep 2014 kl 03:03
+-- Tid vid skapande: 03 okt 2014 kl 13:05
 -- Serverversion: 5.6.15-log
 -- PHP-version: 5.5.8
 
@@ -30,15 +30,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
   `Password` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `UserKey` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=29 ;
 
 --
 -- Dumpning av Data i tabell `user`
 --
 
-INSERT INTO `user` (`ID`, `Username`, `Password`) VALUES
-(1, 'Admin', 'Password');
+INSERT INTO `user` (`ID`, `Username`, `Password`, `UserKey`) VALUES
+(23, 'Admin', 'dc647eb65e6711e155375218212b3964', '19d790cfbf152d494329f2d9f451b136'),
+(25, 'admina', '88fc757bb99a74998a3c7de1b8181c20', 'c4510eda7225c128e8acd0fbf1ba74d7');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
